f69d72b0f914a51d3ee2087711f9f5275e5318b0
Chris Goldsmith <chrisg67@gmail.com>
2013-08-06 07:56:41 +0100

    Make basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -5,14 +5,17 @@ ruby '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 gem 'bootstrap-sass', '2.3.2.0'
+gem 'bcrypt-ruby', '3.0.1'
 
 # Use postgres as the database for Active Record
-group :development, :test do
+group :development do
   gem 'pg', '0.15.1'
   gem 'rspec-rails', '2.13.1'
 end
 
 group :test do
+  gem 'sqlite3', '1.3.7'
+  gem 'rspec-rails', '2.13.1'
   gem 'selenium-webdriver', '2.0.0'
   gem 'capybara', '2.1.0'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,28 +2,64 @@ require 'spec_helper'
 
 describe "StaticPages" do
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it  {should have_title(full_title(page_title))  }
+    it "should have the right links on the layout" do
+      click_link "About"
+      expect(page).to have_title(full_title('About Us'))
+      click_link page_name
+      click_link "Help"
+      expect(page).to have_title(full_title('Help'))
+      click_link page_name
+      click_link "Contact"
+      expect(page).to have_title(full_title('Contact'))
+      click_link page_name
+      click_link "Home"
+      expect(page).to have_title(full_title(''))
+      expect(page).not_to have_title('| Home')
+      click_link page_name
+      click_link "sample app"
+      expect(page).to have_title(full_title(''))
+      expect(page).not_to have_title('| Home')
+    end
+  end
+
   describe "Home page" do
     before { visit root_path }
-    it { should have_content('Sample App') }
-    it  {should have_title(full_title(''))  }
+    let(:heading) {'Sample App'}
+    let(:page_title) {''}
+    let(:page_name) {'Home'}
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
+    it "should have the right link on the sign up button" do
+      click_link "Sign up now!"
+      expect(page).to have_title(full_title('Sign up'))
+    end
   end
 
   describe "Help page" do
     before { visit help_path }
-    it { should have_content('Help') }
-    it  {should have_title(full_title('Help'))  }
+    let(:heading) {'Help'}
+    let(:page_title) {'Help'}
+    let(:page_name) {'Help'}
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should have_content('About') }
-    it  {should have_title(full_title('About Us'))  }
+    let(:heading) {'About'}
+    let(:page_title) {'About Us'}
+    let(:page_name) {'About'}
+    it_should_behave_like "all static pages"
   end
 
   describe "Contacts page" do
     before { visit contact_path }
-    it { should have_content('Contact') }
-    it  {should have_title(full_title('Contact'))  }
+    let(:heading) {'Contact'}
+    let(:page_title) {'Contact'}
+    let(:page_name) {'Contact'}
+    it_should_behave_like "all static pages"
   end
 end
