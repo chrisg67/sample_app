b9b6b128b663cdb6255f2bb6107411c289c29402
Chris Goldsmith <chrisg67@gmail.com>
2013-08-07 11:06:41 +0100

    Finished examples for chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,6 +27,44 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        it { should have_content("Name can't be blank") }
+        it { should have_content("Email can't be blank") }
+        it { should have_content("Email is invalid") }
+        it { should have_content("Password can't be blank") }
+        it { should have_content("Password is too short (minimum is 6 characters)") }
+      end
+    end
+
+    describe "with bad email" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "bademail"
+        fill_in "Password",     with: "password"
+        fill_in "Confirmation", with: "password"
+        click_button submit
+      end
+
+      it { should have_title('Sign up') }
+      it { should have_content('Email is invalid') }
+    end
+
+    describe "with password mismatch" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "email@example.com"
+        fill_in "Password",     with: "password"
+        fill_in "Confirmation", with: "mismatch"
+        click_button submit
+      end
+
+      it { should have_title('Sign up') }
+      it { should have_content("Password confirmation doesn't match Password") }
     end
 
     describe "with valid information" do
@@ -39,6 +77,13 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text:'Welcome') }
       end
     end
   end
